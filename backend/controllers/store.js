4dca87643c68d06ede29a7236b000dc9847929a0
Diana Carolina Chacón López <dchacon871@unab.edu.co>
2021-08-12 11:51:53 -0500

    Update store.js

--- backend/controllers/store.js
+++ backend/controllers/store.js
@@ -4,14 +4,20 @@ const registerStore = async (req, res) => {
   if (!req.body.name || !req.body.city || !req.body.location)
     return res
       .status(401)
-      .send("Please helpe looking you have to fill all the camps noob");
+      .send("const Store = require("../models/store");
+
+const registerStore = async (req, res) => {
+  if (!req.body.name || !req.body.city || !req.body.location)
+    return res
+      .status(401)
+      .send("You must enter all required fields.");
 
   const existingStore = await Store.findOne({ location: req.body.location });
 
   if (existingStore)
     return res
       .status(401)
-      .send("Sorry already have one store in that position");
+      .send("Sorry, already have this store");
 
   const store = new Store({
     name: req.body.name,
@@ -22,7 +28,7 @@ const registerStore = async (req, res) => {
 
   const result = store.save();
 
-  if (!result) return res.status(401).send("Sorry please try again");
+  if (!result) return res.status(401).send("Sorry, please try again");
 
   return res.status(201).send({ store });
 };
@@ -30,7 +36,38 @@ const registerStore = async (req, res) => {
 const listStore = async (req, res) => {
   const store = await Store.find();
 
-  if (!store) return res.status(401).send("Sorry you dont have any products");
+  if (!store) return res.status(401).send("Sorry, you don't have any products");
+
+  return res.status(201).send({ store });
+};
+
+module.exports = { registerStore, listStore };");
+
+  const existingStore = await Store.findOne({ location: req.body.location });
+
+  if (existingStore)
+    return res
+      .status(401)
+      .send("Sorry, already have this store");
+
+  const store = new Store({
+    name: req.body.name,
+    city: req.body.city,
+    location: req.body.location,
+    dbStatus: true,
+  });
+
+  const result = store.save();
+
+  if (!result) return res.status(401).send("Please try again");
+
+  return res.status(201).send({ store });
+};
+
+const listStore = async (req, res) => {
+  const store = await Store.find();
+
+  if (!store) return res.status(401).send("Sorry you don't have any products");
 
   return res.status(201).send({ store });
 };
