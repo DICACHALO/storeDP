76a8e7fcbe729e723f23611a39e7ac6c00a4f310
Diana Carolina Chacón López <dchacon871@unab.edu.co>
2021-08-12 11:06:14 -0500

    Update store.js

--- backend/routes/store.js
+++ backend/routes/store.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const StoreController = require("../controllers/store");
 
 router.post('/registerstore',StoreController.registerStore);
-//http://localhost:3001/api/role/listrole
+
 router.get('/liststore',StoreController.listStore);
 
 //router es el que esta guardando toda la informacion
