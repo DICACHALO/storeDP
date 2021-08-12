a0507b5086e6f8235b60c9739dfb9e79e6e63abe
Diana Carolina Chacón López <dchacon871@unab.edu.co>
2021-08-12 11:23:19 -0500

    Update product.js

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -2,14 +2,14 @@ const Product = require("../models/product");
 
 const registerProduct = async (req, res) => {
   if (!req.body.name || !req.body.code || !req.body.price)
-    return res.status(401).send("You have to fill all the camps so please");
+    return res.status(401).send("You must to register all required fields.");
 
   const existingProduct = await Product.findOne(
     { name: req.body.name } || { code: req.body.code }
   );
 
   if (existingProduct)
-    return res.status(401).send("Sorry you have the same code and name");
+    return res.status(401).send("The product already exists.");
 
   const product = new Product({
     name: req.body.name,
@@ -24,14 +24,14 @@ const registerProduct = async (req, res) => {
   if (!result)
     return res
       .status(401)
-      .send("Sorry I cant save your product please try again");
+      .send("Sorry. The product could not be found, please try again.");
 
   return res.status(201).send({ product });
 };
 
 const listProduct = async (req, res) => {
   const product = await Product.find();
-  if (!product) return res.status(401).send("Sorry I cant find");
+  if (!product) return res.status(401).send("The product doesn't exist.");
   return res.status(201).send({ product });
 };
 
